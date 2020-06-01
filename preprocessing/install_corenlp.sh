0d31f7ba2d1ee202a789129544fab995bcf04291
kurianbenoy <kurian.bkk@gmail.com>
2020-06-02 00:12:17 +0530

    update shell scripts

--- preprocessing/install_corenlp.sh
+++ preprocessing/install_corenlp.sh
@@ -5,23 +5,23 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-set -e
+#set -e
 
 # By default download to the data directory I guess
-read -p "Specify download path or enter to use default (../data/corenlp): " path
+#read -p "Specify download path or enter to use default (../data/corenlp): " path
 DOWNLOAD_PATH="${path:-../data/corenlp}"
-echo "Will download to: $DOWNLOAD_PATH"
+#echo "Will download to: $DOWNLOAD_PATH"
 
 # Download zip, unzip
-pushd "/tmp"
-wget -O "stanford-corenlp-full-2017-06-09.zip" "http://nlp.stanford.edu/software/stanford-corenlp-full-2017-06-09.zip"
-unzip "stanford-corenlp-full-2017-06-09.zip"
-rm "stanford-corenlp-full-2017-06-09.zip"
-popd
+#pushd "/tmp"
+#wget -O "stanford-corenlp-full-2017-06-09.zip" "http://nlp.stanford.edu/software/stanford-corenlp-full-2017-06-09.zip"
+#unzip "stanford-corenlp-full-2017-06-09.zip"
+#rm "stanford-corenlp-full-2017-06-09.zip"
+#popd
 
 # Put jars in DOWNLOAD_PATH
-mkdir -p "$DOWNLOAD_PATH"
-mv "/tmp/stanford-corenlp-full-2017-06-09/"*".jar" "$DOWNLOAD_PATH/"
+#mkdir -p "$DOWNLOAD_PATH"
+#mv "/tmp/stanford-corenlp-full-2017-06-09/"*".jar" "$DOWNLOAD_PATH/"
 
 # Append to bashrc, instructions
 while read -p "Add to ~/.bashrc CLASSPATH (recommended)? [yes/no]: " choice; do
